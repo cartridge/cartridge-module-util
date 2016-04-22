dd17976dd95837695ac2dc97e5b2d42c7d8230e2
tawashley <trisashley@gmail.com>
2016-04-22 11:50:25 +0100

    test: Add mock package config. Add tests to check module exports correct data types

--- test/moduleUtilsSpec.js
+++ test/moduleUtilsSpec.js
@@ -1,13 +1,26 @@
+var fs = require('fs');
+var path = require('path');
+
 var chai = require('chai');
 var expect = chai.expect;
+var mockPackageJson = JSON.parse(fs.readFileSync(path.join(__dirname, './mockPackageConfig.json'), 'utf8'));
+var moduleUtils = require('../index.js');
+var moduleUtilsInstance = moduleUtils(mockPackageJson);
 
 chai.use(require('chai-fs'));
 chai.should();
 
-describe('As placehoder test', function() {
+describe('As user of the module utils module', function() {
 
-	it('should assert true is true', function() {
-		true.should.be.true;
+	it('should export a function', function() {
+		expect(moduleUtils).to.be.an('function');
+	})
+
+	describe('When creating an instance', function() {
+
+		it('should return an object', function() {
+			expect(moduleUtilsInstance).to.be.an('object');
+		})
 	})
 
 })
